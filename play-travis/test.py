abca08df8ff30b0fcf9103c9e2f1be9ab645f354
Fedor Obolihin <fedor.obolihin@zyfra.com>
2023-10-19 14:19:10 +0300

    fix test

--- play-travis/test.py
+++ play-travis/test.py
@@ -4,7 +4,9 @@ import unittest
 class NumbersTest(unittest.TestCase):
 
     def test_equal(self) -> None:
-        self.assertEqual(1 + 1, 1)
+        result = 1 + 1
+        expected = 2
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
